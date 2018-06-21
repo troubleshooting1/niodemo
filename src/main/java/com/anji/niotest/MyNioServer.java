d40a12a5e35f630a018a4ecfb6df34afcff0a632
troubleshooting1 <467503768@qq.com>
2018-06-21 14:40:55 +0800

    添加一些注释

--- src/main/java/com/anji/niotest/MyNioServer.java
+++ src/main/java/com/anji/niotest/MyNioServer.java
@@ -11,7 +11,11 @@ import java.util.Iterator;
 import java.util.Set;
 
 /**
- * Description:
+ * Description:阻塞体现在这个线程不能干别的了，只能在这里等着。非阻塞体现在这个线程可以去干别的，不需要一直在这等着。
+ * 说NIO的非阻塞原理之前，我们需要先说一下传统的io。传统的IO是按字节传输的，即每次传输一个字节。
+ * 为了提高数据传输效率，引进了带缓冲区得输入输出模式，这样每次就可以传输大量的字节数，
+ * 但是，会导致在读(写)缓冲区没有满的情况下，程序会一直等待，直到满或者关闭流才能读取（写入）。
+ * 这样导致程序阻塞，降低了程序的执行效率。
  * author: chenqiang
  * date: 2018/6/21 10:50
  */
@@ -31,7 +35,7 @@ public class MyNioServer {
         SelectionKey selectionKey=channel.register(selector,SelectionKey.OP_ACCEPT);
 
         while (true){                       //轮询
-            selector.select();              //这是一个阻塞方法，一直等待直到有数据可读，返回值是key的数量（可以有多个）
+            selector.select();              //!!!!!!!!!!!!!!!这是一个阻塞方法，一直等待直到有数据可读，返回值是key的数量（可以有多个）
             Set keys=selector.selectedKeys();           //如果channel有数据了，将生成的key放入Keys集合
             Iterator iterator=keys.iterator();            //得到这个keys的迭代器
             while (iterator.hasNext()){
@@ -54,7 +58,7 @@ public class MyNioServer {
     public void doAccept(SelectionKey key) throws IOException{
         ServerSocketChannel serverSocketChannel=(ServerSocketChannel)key.channel();
         System.out.println("ServerSocketChannel正在循环监听");
-        SocketChannel clientChannel=serverSocketChannel.accept();
+        SocketChannel clientChannel=serverSocketChannel.accept();       // 获得和客户端连接的通道
 
         clientChannel.configureBlocking(false);
         clientChannel.register(key.selector(),SelectionKey.OP_READ);
@@ -70,7 +74,7 @@ public class MyNioServer {
             String info=new String(data).trim();
             System.out.println("从客户端发送过来的消息是："+info);
             byteBuffer.clear();
-            bytesRead=clientChannel.read(byteBuffer);
+            bytesRead=clientChannel.read(byteBuffer);           //在和客户端连接成功之后，为了可以接收到客户端的信息，需要给通道设置读的权限
         }
         if(bytesRead==1){
             clientChannel.close();
